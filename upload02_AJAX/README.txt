2f1b5e8eb13c57b69d060d475896345bb6b28721
unknown <abrady@.Us.Atari.loc>
2010-12-13 18:15:32 -0800

    more upload

---
+++ upload02_AJAX/README.txt
@@ -0,0 +1,5 @@
+Async file upload in pure javascript.
+
+From here:
+- http://igstan.ro/posts/2009-01-11-ajax-file-upload-with-pure-javascript.html
+- https://github.com/igstan/ajax-file-upload
---
+++ upload02_AJAX/upload.php
@@ -0,0 +1,100 @@
+<?php
+
+// print the uploaded information
+// print_r($_FILES); // <= assoc array by form id (i.e. photo) and then an array of file info
+// print_r($_POST); // <= other posted info: image_name, image_type
+$upload_dir = 'uploads/';
+
+$upload_file = $upload_dir . basename($_FILES['photo']['name']);
+	
+if (!preg_match("/(gif|jpg|jpeg|png)$/",$_FILES['photo']['name'])) {
+	print $upload_file . " : doesn't appear to be an image";
+} else {
+	if (!is_uploaded_file($_FILES['photo']['tmp_name']))  {
+		print "file " . $upload_file . " wasn't a file that was uploaded";
+	} else if (!move_uploaded_file($_FILES['photo']['tmp_name'], $upload_file)) {
+		print "couldn't move file " . $upload_file;
+	}
+	else {
+		print "success!";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
